7c3de8f486047e2fe624a3668039516005a805f8
Fesposito9197 <kekkoesp8@gmail.com>
2022-10-21 16:31:29 +0200

    js finito

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const genera = document.getElementById('genera');
 
 genera.addEventListener('click' , function( ){
-    const passangerInfo = document.getElementById('nome-passeggero').value;
+    const passangerInfo = document.getElementById('nome').value;
     console.log(passangerInfo);
     const passangerKm = document.getElementById('km-passeggero').value;
     console.log(passangerKm);
@@ -20,8 +20,41 @@ genera.addEventListener('click' , function( ){
     } else if (passangerAge=="minorenne"){
         ticketPrice -= ticketPrice * 0.2
         document.getElementById("offerta").innerHTML = "Offerta young"
-    } else if (passangerInfo=="over65"){
+        document.getElementById("biglietto").className=("show-ticket")
+    } else if (passangerAge=="over65"){
         ticketPrice-= ticketPrice * 0.4
         document.getElementById("offerta").innerHTML = "Offerta silver"
+        document.getElementById("biglietto").className=("show-ticket")
+
+    } else {
+        ticketPrice = ticketPrice
+        document.getElementById("offerta").innerHTML = "offerta standard"
+        document.getElementById("biglietto").className=("show-ticket")
+
     }
+
+    // genero il numero carrozza e il codice cp
+    const carrozza = Math.floor(Math.random() * 10) + 1;
+    const codice = Math.floor(Math.random() * 9999) + 90000;
+    console.log(carrozza)
+    console.log(codice)
+
+   
+    
+
+    // biglietto completo
+    document.getElementById("nome-passeggero").innerHTML = passangerInfo
+    document.getElementById("carrozza").innerHTML = carrozza
+    document.getElementById("codice").innerHTML = codice
+    document.getElementById("costo").innerHTML = ticketPrice.toFixed(2) + "€"
+});
+
+
+// tasto annulla
+
+const annulla = document.getElementById("annulla")
+
+annulla.addEventListener("click", function(){
+    window.location.reload()
 })
+
